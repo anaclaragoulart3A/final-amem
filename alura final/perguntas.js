ad1e8295e32c868be5a9e223e264f99ed20bbd4a
Ana Clara Goulart <goulartoanaclara@gmail.com>
2024-11-24 10:27:24 -0300

    Add files via upload

---
+++ alura final/perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+function criaCartao(categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
